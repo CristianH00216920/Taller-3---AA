d025de823d5d664187755d453fdd86843a1e6ba2
CristianH00216920 <00216920@uca.edu.sv>
2022-11-17 16:44:05 -0600

    ejec 4 - final

--- Ejercicio4.cpp
+++ Ejercicio4.cpp
@@ -4,53 +4,53 @@
 
 using namespace std;
 
-int coinExchange(int *coins, int target, int coinsNumb)
+int withDrawCash(int *dollarsBills, int balance, int numb)
 {
 
-	int T[target + 1];
+    int T[balance + 1];
 
-	T[0] = 0;
+    T[0] = 0;
 
-	for (int i = 1; i <= target; i++)
-	{
+    for (int i = 1; i <= balance; i++)
+    {
 
-		T[i] = INT_MAX;
-		int result = INT_MAX;
+        T[i] = INT_MAX;
+        int result = INT_MAX;
 
-		for (int c = 0; c < coinsNumb; c++)
-		{
-			if (i - coins[c] >= 0)
-			{
-				result = T[i - coins[c]];
-			}
+        for (int j = 0; j < numb; j++)
+        {
+            if (i - dollarsBills[j] >= 0)
+            {
+                result = T[i - dollarsBills[j]];
+            }
 
-			if (result != INT_MAX)
-			{
-				T[i] = min(T[i], result + 1);
-			}
-		}
-	}
+            if (result != INT_MAX)
+            {
+                T[i] = min(T[i], result + 1);
+            }
+        }
+    }
 
-	return T[target];
+    return T[balance];
 }
 
 int main()
 {
-	int coins[] = {1, 5, 10, 20, 100}; 
+    int dollarsBills[] = {1, 5, 10, 20, 100}; // denomination of dollars, arrays for values to use.
 
-	int target; 
-	cout << "Ingrese cantidad de dolares: ";
-	cin >> target;
+    int balance; // amoung of dollars that will be withdraw
+    cout << "Ingrese cantidad de dolares: ";
+    cin >> balance;
 
-	if (target >= 1 && target <= pow(10, 9))
-	{
-		int coin = coinExchange(coins, target, 5);
-		cout << "El numero de monedas minimo es: " << coin;
-	}
-	else
-	{
-		cout << "\nNo se puede procesar el monto. ";
-	}
+    if (balance >= 1 && balance <= pow(10, 9))
+    {
+        int dollar = withDrawCash(dollarsBills, balance, 5);
+        cout << "El numero de monedas minimo es: " << dollar;
+    }
+    else
+    {
+        cout << "\nNo se puede procesar el monto. ";
+    }
 
-	return 0;
+    return 0;
 }
